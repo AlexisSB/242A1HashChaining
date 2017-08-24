63aa83c01aa7840132e782d047ca215a9c502830
Alexis Barltrop <alexisbarltrop@gmail.com>
2017-08-24 16:58:21 +1200

    add blank files, add checklist, add makefile and test travis

---
+++ asgn.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "container.h"
+#include "flexarray.h"
+#include "htable.h"
+#include "mylib.h"
+#include "rbt.h"
+
+int main(void){
+    printf("Hello World!");
+    return EXIT_SUCCESS;
+    
+}
+        
---
+++ makefile
@@ -0,0 +1,2 @@
+asgn: asgn.c container.c flexarray.c htable.c mylib.c rbt.c
+	gcc -W -Wall -O2 -ansi - pedantic -lm *.c -o asgn
