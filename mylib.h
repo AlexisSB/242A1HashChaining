fccb589016195e671073b9e7c090f364fc3a0baf
Ryan Collins <colry349@gmail.com>
2017-09-14 13:33:07 +1200

    Perfunctory comments for mylib.h

--- mylib.h
+++ mylib.h
@@ -8,9 +8,20 @@
 #define MYLIB_H_
 #include <stddef.h>
 
+/* Allocates memory with error checking.
+   (see mylib.c for more information) */
 extern void *emalloc(size_t size);
+
+/* Reallocates memory with error checking.
+   (see mylib.c for more information) */
 extern void *erealloc(void *p, size_t s);
+
+/* Prints a container key to stdout.
+   (see mylib.c for more information) */
 extern void print_key(char* str);
+
+/* Gets a string of from a stream.
+   (see mylib.c for more information) */
 extern int getword(char *s, int limit, FILE *stream);
 
 #endif
