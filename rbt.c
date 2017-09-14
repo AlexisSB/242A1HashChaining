abcb84558856812976351c36e7e216a9443be12c
Ryan Collins <colry349@gmail.com>
2017-09-14 13:27:06 +1200

    Comments, consistent param names, tidied free()

--- rbt.c
+++ rbt.c
@@ -4,11 +4,12 @@
 #include "mylib.h"
 #include "rbt.h"
 
+/* Macros for determining the colour of an rbt node. */
 #define IS_BLACK(x) ((NULL == (x)) || (BLACK == (x)->colour))
 #define IS_RED(x) ((NULL != (x)) && (RED == (x)->colour))
 
+/* Enum for the possible colour values of each rbt node. */
 typedef enum { RED, BLACK } rbt_colour;
-
 
 /* Red Black Tree node struct.
  * key - root node key
@@ -21,8 +22,6 @@ struct rbtnode {
     rbt_colour colour;
     rbt left, right;
 };
-
-
 
 /* Allocates memory for a new red-black tree.
  * Sets key, left, and right to null. 
@@ -37,47 +36,67 @@ rbt rbt_new() {
     return result;
 }
 
-void rbt_preorder(rbt b, void function(char* str)) {
-    if (b == NULL || strcmp(b->key, "") == 0) return;
-    function(b->key);
-    rbt_preorder(b->left, function);
-    rbt_preorder(b->right, function);
+/* Preorder traverses a specified red-black tree.
+ * Calls a specified function on the key of each node.
+ * @param r Red-black tree to traverse
+ * @param function Function to be called on each node's key
+ */
+void rbt_preorder(rbt r, void function(char* str)) {
+    if (r == NULL || strcmp(r->key, "") == 0) return;
+    function(r->key);
+    rbt_preorder(r->left, function);
+    rbt_preorder(r->right, function);
 }
 
+/* Prints each node of a specified red-black tree to stdout.
+ * Nodes are printed in preorder.
+ * @param r Red-black tree to be printed
+ */
 void rbt_print(rbt r) {
     if (r == NULL) return;
     rbt_preorder(r, print_key);
     fprintf(stdout, "\n");
 }
 
-int rbt_search(rbt b, char* str) {
+/* Search a specified red-black tree for a specified value.
+   @param r Red-black tree to be searched
+   @param str String to search for
+   @return int Returns 1 if the value is found, 0 if not
+ */
+int rbt_search(rbt r, char* str) {
     int compare = 0;
-    if (b == NULL || strcmp(b->key, "") == 0) {
+    if (r == NULL || strcmp(r->key, "") == 0) {
         return 0;
     }
-    compare = strcmp(b->key, str);
+    compare = strcmp(r->key, str);
     if (compare == 0) {
         return 1;
     } else if (compare > 0) {
-        return rbt_search(b->left, str);
+        return rbt_search(r->left, str);
     } else {
-        return rbt_search(b->right, str);
+        return rbt_search(r->right, str);
     }
 }
 
-rbt rbt_free(rbt b) {
-    if (b == NULL) {
-        return NULL;
+/* Frees all memory associated with a specified red-black tree.
+ * @param r Red-black tree to free
+ * @return Always returns NULL
+ */
+rbt rbt_free(rbt r) {
+    if (r != NULL) {
+        rbt_free(r->left);
+        rbt_free(r->right);
+        if (r->key != NULL) {
+            free(r->key);
+        }
+        free(r);
     }
-    rbt_free(b->left);
-    rbt_free(b->right);
-    if (b->key != NULL) {
-        free(b->key);
-    }
-    free(b);
     return NULL;
 }
 
+/* Rotates a specified red-black tree left.
+ * @param r Red-black tree to rotate
+ */
 rbt left_rotate(rbt r) {
     rbt temp = r;
     r = r->right;
@@ -86,6 +105,9 @@ rbt left_rotate(rbt r) {
     return r;
 }
 
+/* Rotates a specified red-black tree right.
+   @param r Red-black tree to rotate
+ */
 rbt right_rotate(rbt r) {
     rbt temp = r;
     r = r->left;
@@ -94,8 +116,12 @@ rbt right_rotate(rbt r) {
     return r;
 }
 
+/* Performs rotations and recolourings required to
+ * maintain the black-height properties of a
+ * specified red-black tree.
+ * @param r Red-black tree to fix
+ */
 rbt rbt_fix(rbt r) {
-    
     if (r->left != NULL && IS_RED(r->left)) {/*Has a red left child*/
         if (IS_RED(r->left->left)) {/*left left is red*/
             if (IS_RED(r->right)) {/*right is black*/
@@ -157,32 +183,43 @@ rbt rbt_fix(rbt r) {
     return r;
 }
 
-rbt rbt_do_insertion(rbt b, char* str){
-    /* this is here because setting the root colour is
-       problematic with recursion */
-    b = rbt_insert(b, str);
-    b->colour = BLACK;
-    return b;
-}
-
-rbt rbt_insert(rbt b, char* str) {
+/* Inserts a specified string to into a specified red-black tree.
+ * To maintain all red-black properties, this function should
+ * only be called through rbt_do_insertion.
+ * @param r Red-black tree to insert into
+ * @param str String value to insert
+ * @return rbt The new state of the red-black tree
+ */
+rbt rbt_insert(rbt r, char* str) {
     int compare = 0;
-    if (b == NULL){
-        b = rbt_new();
+    if (r == NULL){
+        r = rbt_new();
     }
-    if (b->key == NULL) {
-        b->key = emalloc((strlen(str) + 1) * sizeof(char));
-        strcpy(b->key, str);
-        b->colour = RED;
+    if (r->key == NULL) {
+        r->key = emalloc((strlen(str) + 1) * sizeof(char));
+        strcpy(r->key, str);
+        r->colour = RED;
     } else {
-        compare = strcmp(b->key, str);
+        compare = strcmp(r->key, str);
         if (compare >= 0) {
-            b->left = rbt_insert(b->left, str);
+            r->left = rbt_insert(r->left, str);
         } else {
-            b->right = rbt_insert(b->right, str);
+            r->right = rbt_insert(r->right, str);
         }
     }
     
-    b = rbt_fix(b);
-    return b;
+    r = rbt_fix(r);
+    return r;
+}
+
+/* Inserts a specified string value into a specified red-black tree.
+ * Red-black properties are maintained.
+ * @param r Red-black tree to insert into
+ * @param str String value to insert
+ * @return rbt The new state of the red-black tree
+ */
+rbt rbt_do_insertion(rbt r, char* str){
+    r = rbt_insert(r, str);
+    r->colour = BLACK;
+    return r;
 }
--- rbt.h
+++ rbt.h
@@ -1,13 +1,28 @@
 #ifndef RBT_H_
 #define RBT_H_
 
+/* Header file for rbt.c */
+
 typedef struct rbtnode *rbt;
 
+/* Creates a new rbt
+   (see rbt.c for more information) */
 extern rbt rbt_new();
+
+/* Frees all memory associated with an rbt
+   (see rbt.c for more information) */
 extern rbt rbt_free(rbt r);
+
+/* Inserts a string into an rbt
+   (see rbt.c for more information) */
 extern rbt rbt_do_insertion(rbt r, char* str);
-extern rbt rbt_insert(rbt r, char* str);
+
+/* Searches an rbt for a specified string
+   (see rbt.c for more information) */
 extern int rbt_search(rbt r, char* str);
+
+/* Preorder prints the key of each node in an rbt
+   (see rbt.c for more information) */
 extern void rbt_print(rbt r);
 
 #endif
