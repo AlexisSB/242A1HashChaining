4b8d36d6a59651422ed0236f162184bca8e51243
rycoll <colry349@gmail.com>
2017-09-15 12:08:24 +1200

    Capitalised comments, added fullstops, moved */

--- container.h
+++ container.h
@@ -3,29 +3,29 @@
 
 #include <stddef.h>
 
-/* header file for container.c */
+/* Header file for container.c*/
 
 typedef enum container_e {FLEX_ARRAY, RED_BLACK_TREE} container_t;
 typedef struct containerrec *container;
 
-/* creates new (empty) container
-   (see container.c for more information) */
+/* Creates new (empty) container.
+   (see container.c for more information)*/
 extern container container_new(container_t container_type);
 
-/* frees all memory associated with container
-   (see container.c for more information) */
+/* Frees all memory associated with container.
+   (see container.c for more information)*/
 extern void container_free(container c);
 
-/* adds string to container (rbt/flexarray)
-   (see container.c for more information) */
+/* Adds string to container (rbt/flexarray).
+   (see container.c for more information)*/
 extern void container_add(container c, char* word);
 
-/* searches for string in container
-   (see container.c for more information) */
+/* Searches for string in container.
+   (see container.c for more information)*/
 extern int container_search(container c, char* str);
 
-/* prints every string in the container
-   (see container.c for more information) */
+/* Prints every string in the container.
+   (see container.c for more information)*/
 extern void container_print(container c);
 
 
