ae8dd57a7e9262a74a6b70cbfa85f85812a91bbe
rycoll <colry349@gmail.com>
2017-09-15 12:04:04 +1200

    Capitalised comments, added fullstops

--- flexarray.h
+++ flexarray.h
@@ -1,27 +1,27 @@
 #ifndef FLEXARRAY_H_
 #define FLEXARRAY_H_
 
-/* header file for flexarray (of strings)  */
+/* Header file for flexarray (of strings)  */
 
 typedef struct flexarrayrec *flexarray;
 
-/* appends string to flexarray
+/* Appends string to flexarray.
    (see flexarray.c for more information) */
 extern void flexarray_append(flexarray f, char* item);
 
-/* frees all memory associated with flexarray
+/* Frees all memory associated with flexarray.
    (see flexarray.c for more information) */
 extern void flexarray_free(flexarray f);
 
-/* creates new (empty) flexarray
+/* Creates new (empty) flexarray.
    (see flexarray.c for more information) */
 extern flexarray flexarray_new();
 
-/* searches for a string in a flexarray
+/* Searches for a string in a flexarray.
    (see flexarray.c for more information) */
 extern int is_present(flexarray f, char* str);
 
-/* applies a function to every item in a flexarray
+/* Applies a function to every item in a flexarray.
    (see flexarray.c for more information) */
 extern void visit(flexarray a, void f(char* str));
 
