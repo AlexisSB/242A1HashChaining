35931f5c39c9fcadf1f269feb2cc57bbee983ed5
rycoll <colry349@gmail.com>
2017-09-15 12:12:45 +1200

    Added fullstops to comments, moved */

--- rbt.h
+++ rbt.h
@@ -1,28 +1,28 @@
 #ifndef RBT_H_
 #define RBT_H_
 
-/* Header file for rbt.c */
+/* Header file for rbt.c*/
 
 typedef struct rbtnode *rbt;
 
-/* Creates a new rbt
-   (see rbt.c for more information) */
+/* Creates a new rbt.
+   (see rbt.c for more information)*/
 extern rbt rbt_new();
 
-/* Frees all memory associated with an rbt
-   (see rbt.c for more information) */
+/* Frees all memory associated with an rbt.
+   (see rbt.c for more information)*/
 extern rbt rbt_free(rbt r);
 
-/* Inserts a string into an rbt
-   (see rbt.c for more information) */
+/* Inserts a string into an rbt.
+   (see rbt.c for more information)*/
 extern rbt rbt_do_insertion(rbt r, char* str);
 
-/* Searches an rbt for a specified string
-   (see rbt.c for more information) */
+/* Searches an rbt for a specified string.
+   (see rbt.c for more information)*/
 extern int rbt_search(rbt r, char* str);
 
-/* Preorder prints the key of each node in an rbt
-   (see rbt.c for more information) */
+/* Preorder prints the key of each node in an rbt.
+   (see rbt.c for more information)*/
 extern void rbt_print(rbt r);
 
 #endif
